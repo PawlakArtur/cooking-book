f94bdfb4a2fb5fa21f7c23655719ad4577bb5509
Artur Pawlak <pawlak.artur90@gmail.com>
2018-09-15 20:13:31 +0200

    Add React router to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import { RecipeAdd, RecipeDetails, RecipesList, SignUp } from './views';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav>
+          <Link to="/recipeAdd">recipeAdd</Link>
+          <Link to="/recipeDetails">recipeDetails</Link>
+          <Link to="/recipesList">recipesList</Link>
+          <Link to="/signUp">SignUp</Link>
+        </nav>
+        <div>
+          <Route path="/recipeAdd" component={RecipeAdd}/>
+          <Route path="/recipeDetails" component={RecipeDetails}/>
+          <Route path="/recipesList" component={RecipesList}/>
+          <Route path="/signUp" component={SignUp}/>
+        </div>
       </div>
     );
   }
