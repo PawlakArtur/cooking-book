029ffc8cd7111959af9d26b1aaee11ea19b2577f
Artur Pawlak <pawlak.artur90@gmail.com>
2018-09-15 20:27:45 +0200

    Handle sign up functionality

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,13 +1,88 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { auth } from '../firebase';
+
+const INITIAL_STATE = {
+    userName: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    error: null
+}
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+        this.onSubmit = this.onSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    onSubmit(e) {
+        const { email, password } = this.state;
+        const { history } = this.props;
+        
+        auth.doCreateUserWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                history.push('/');
+            })
+            .catch(error => {
+                this.setState({ error: error });
+            });
+        
+        e.preventDefault();
+    }
+
+    handleInput(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
+        const { userName, email, password, passwordConfirmation, error } = this.state;
+        const isInvalid =
+            password !== passwordConfirmation ||
+            password === '' ||
+            email === '' ||
+            userName === '';
         return (
             <div>
-                SignUp
+                <h1>Signup</h1>
+                <form onSubmit={this.onSubmit}>
+                    <input
+                        type="text"
+                        onChange={this.handleInput}
+                        value={userName}
+                        placeholder="User name"
+                        name="userName"/>
+                    <input
+                        type="text"
+                        onChange={this.handleInput}
+                        value={email}
+                        placeholder="Address email"
+                        name="email"/>
+                    <input
+                        type="password"
+                        onChange={this.handleInput}
+                        value={password}
+                        placeholder="Password"
+                        name="password"/>
+                    <input
+                        type="password"
+                        onChange={this.handleInput}
+                        value={passwordConfirmation}
+                        placeholder="Confirm password"
+                        name="passwordConfirmation"/>
+                    <button
+                        type="submit"
+                        disabled={isInvalid}>
+                        Sign up
+                    </button>
+                </form>
+                { error && <p>{error.message}</p>}
             </div>
     );
   }
 }
 
-export default SignUp;
+export default withRouter(SignUp);
