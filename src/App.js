4429e29dda82ebd51abd37f4ddff2037fea3a194
Artur Pawlak <pawlak.artur90@gmail.com>
2018-09-16 11:29:17 +0200

    Use Navigation component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { RecipeAdd, RecipeDetails, RecipesList, SignUp, SignIn } from './views';
-import { SignOutButton } from './components';
+import { Navigation } from './components';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <Link to="/recipeAdd">recipeAdd</Link>
-          <Link to="/recipeDetails">recipeDetails</Link>
-          <Link to="/recipesList">recipesList</Link>
-          <Link to="/signUp">SignUp</Link>
-          <Link to="/signIn">SignIn</Link>
-          <SignOutButton/>
-        </nav>
+        <Navigation/>
         <div>
           <Route path="/recipeAdd" component={RecipeAdd}/>
           <Route path="/recipeDetails" component={RecipeDetails}/>
