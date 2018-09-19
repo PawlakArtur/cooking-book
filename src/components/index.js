3557c422ae8d0896c9a29fce3c98caf297f90311
Artur Pawlak <pawlak.artur90@gmail.com>
2018-09-19 17:42:18 +0200

    Add Authorization component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import SignOutButton from './SignOutButton';
 import Navigation from './Navigation';
 import withAuthentication from './withAuthentication';
 import AuthUserContext from './AuthUserContext';
+import withAuthorization from './withAuthorization';
 
 export {
     SignOutButton,
     Navigation,
     withAuthentication,
-    AuthUserContext
+    AuthUserContext,
+    withAuthorization
 };
