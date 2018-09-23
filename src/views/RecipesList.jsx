f365be6c7af40b8cf6287812e0fb25f93cfbec10
Artur Pawlak <pawlak.artur90@gmail.com>
2018-09-23 19:32:29 +0200

    Update RecipesList view. List all recipes for current logged user

--- src/views/RecipesList.jsx
+++ src/views/RecipesList.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import { auth, store } from '../firebase';
+import { withAuthorization } from '../components';
+import { Link } from 'react-router-dom';
+
+const INITIAL_STATE = {
+	recipes: [],
+	error: null
+};
 
 class RecipesList extends Component {
+	constructor() {
+		super();
+		this.state = { ...INITIAL_STATE };
+	}
+
+	componentDidMount() {
+		const currentUserUID = auth.getCurrentUserUID();
+		store.getRecipes(currentUserUID)
+			.then(snapshot => {
+				const recipes = [];
+				Object.keys(snapshot.val()).forEach(recipeID => {
+					const recipe = Object.assign(snapshot.val()[recipeID], { recipeID: recipeID });
+					recipes.push(recipe);
+				});
+				this.setState({ recipes });
+			})
+			.catch(error => {
+				this.setState({ error });
+			});
+	}
+
 	render() {
+		const { error, recipes } = this.state;
+		console.log(recipes);
 		return (
 			<div>
-                RecipesList
+				<h1>Recipes list:</h1>
+				<ul>
+					{recipes.map((recipe, index) =>
+						<li key={index}>
+							<Link to={`/recipeDetails/${recipe.recipeID}`}>{recipe.name}</Link>
+						</li>
+					)}
+				</ul>
+				{ error && <p>{error.message}</p>}
 			</div>
 		);
 	}
 }
 
-export default RecipesList;
+const authCondition = authUser => Boolean(authUser);
+
+export default withAuthorization(authCondition)(RecipesList);
