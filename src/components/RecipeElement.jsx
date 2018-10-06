b827bdf7fd444366fa84ab8a67ca70fd73455aa4
Artur Pawlak <pawlak.artur90@gmail.com>
2018-10-06 19:06:18 +0200

    Display addition data in recipe element

--- src/components/RecipeElement.jsx
+++ src/components/RecipeElement.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import DataContext from './DataContext';
 
 const RecipeElement = props =>
-	<li className="recipe__element">
-		<Link className="recipe__link" to={`/recipeDetails/${props.recipe.recipeID}`}>{props.recipe.name}</Link>
-	</li>;
+	<DataContext.Consumer>
+		{ ({ categoryList }) => {
+			const cateogryName = categoryList.find(cateogry => cateogry.id === props.recipe.categoryID).name;
+			return <li className="recipe__element">
+				<Link className="recipe__link" to={`/recipeDetails/${props.recipe.recipeID}`}>
+					<p>{props.recipe.name}</p>
+					<p>Execution time: {props.recipe.executionTime}</p>
+					<p>Category: {cateogryName}
+					</p>
+				</Link>
+			</li>;
+		}}
+	</DataContext.Consumer>;
 
 export default RecipeElement;
 
 RecipeElement.propTypes = {
 	recipe: PropTypes.shape({
 		recipeID: PropTypes.string,
-		name: PropTypes.string
+		name: PropTypes.string,
+		executionTime: PropTypes.string,
+		categoryID: PropTypes.string
 	})
 };
