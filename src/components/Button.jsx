8c04a5bc6156c9bcbf5230d7c471d3b200fdb446
Artur Pawlak <pawlak.artur90@gmail.com>
2018-10-21 17:46:22 +0200

    Update Button component. Handle type submit for #13

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -16,22 +16,28 @@ LinkButton.propTypes = {
 	to: PropTypes.string
 };
 
-const buttonFunction = (actionFunction, e) => {
-	e.preventDefault();
-	actionFunction();
+const buttonFunction = (actionFunction, type, e) => {
+	if (type !== 'submit') {
+		e.preventDefault();
+	}
+	if (actionFunction) {
+		actionFunction();
+	}
 };
 
-const ActionButton = ({ actionFunction, children, cssClass }) =>
+const ActionButton = ({ actionFunction, children, cssClass, type }) =>
 	<button
 		className={`button__button ${cssClass}`}
-		onClick={buttonFunction.bind(null, actionFunction)}>
+		onClick={buttonFunction.bind(null, actionFunction, type)}
+		type={type}>
 		{children}
 	</button>;
 
 ActionButton.propTypes = {
 	actionFunction: PropTypes.func,
 	children: PropTypes.string,
-	cssClass: PropTypes.string
+	cssClass: PropTypes.string,
+	type: PropTypes.string
 };
 
 const Button = props =>
@@ -44,7 +50,16 @@ Button.propTypes = {
 	children: PropTypes.string.isRequired,
 	cssClass: PropTypes.string,
 	linkButton: PropTypes.bool,
-	to: PropTypes.string
+	to: PropTypes.string,
+	type: PropTypes.string
+};
+
+Button.defaultProps = {
+	actionFunction: null,
+	cssClass: '',
+	linkButton: false,
+	to: null,
+	type: 'button'
 };
 
 export default Button;
