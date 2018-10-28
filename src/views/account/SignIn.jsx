a92e0c7f67fb7c965151ac83a40b88b6a37babcf
Artur Pawlak <pawlak.artur90@gmail.com>
2018-10-28 19:37:43 +0100

    Use new HOC to get translation props in 'signIn' view for #15

--- src/views/account/SignIn.jsx
+++ src/views/account/SignIn.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import { auth } from '../../firebase';
-import { Button } from '../../components';
+import { Button, withRouterWrapper } from '../../components';
 
 const INITIAL_STATE = {
 	email: '',
@@ -40,30 +39,31 @@ class SignIn extends Component {
 
 	render() {
 		const { email, password, error } = this.state;
+		const { translate } = this.props;
 		const isInvalid = email === '' || password === '';
 		return (
 			<div className="layout__container layout__container--main">
-				<h1 className="layout__title">SignIn</h1>
+				<h1 className="layout__title">{translate('views.signIn')}</h1>
 				<form className="layout__main layout__container layout__container--form form__container" onSubmit={this.onSubmit}>
 					<input
 						type="text"
 						onChange={this.handleInput}
 						value={email}
-						placeholder="Email address"
+						placeholder={translate('views.emailAddress')}
 						name="email"
 						className="form__input"/>
 					<input
 						type="password"
 						onChange={this.handleInput}
 						value={password}
-						placeholder="Password"
+						placeholder={translate('views.password')}
 						name="password"
 						className="form__input"/>
 					<Button
 						type="submit"
 						cssClass="layout__button"
 						disabled={isInvalid}>
-                        Sign in
+						{translate('views.signIn')}
 					</Button>
 				</form>
 				{ error && <p>{error.message}</p> }
@@ -72,8 +72,9 @@ class SignIn extends Component {
 	}
 }
 
-export default withRouter(SignIn);
+export default withRouterWrapper(SignIn);
 
 SignIn.propTypes = {
-	history: PropTypes.object
+	history: PropTypes.object.isRequired,
+	translate: PropTypes.func.isRequired
 };
