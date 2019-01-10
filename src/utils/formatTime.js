a78948d7f537f1f8521b71bc9b7614858eb8ae9a
Artur Pawlak <pawlak.artur90@gmail.com>
2019-01-10 17:49:50 +0100

    Fix calculating formatTime util

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -2,7 +2,7 @@ const formatTime = function (inputTime) {
 	const formattedHours = inputTime / 60;
 	const formattedMinutes = inputTime % 60;
 	const formattedTime = {
-		hours: formattedHours,
+		hours: formattedHours < 1 ? 0 : formattedHours,
 		minutes: formattedMinutes
 	};
 	return formattedTime;
