5a17666e454b6c478eb6afe2a7d97c0ff8295a31
Artur Pawlak <pawlak.artur90@gmail.com>
2019-01-10 14:39:59 +0100

    Add css styles for SignOutButton

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { auth } from '../firebase';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const SignOutButton = ({ translate }) =>
-	<button
-		type="button"
-		onClick={auth.doSignOut}>
+	<Button
+		actionFunction={auth.doSignOut}
+		cssClass="button__button--transparent">
 		{translate('components.signOut')}
-	</button>;
+	</Button>;
 
 SignOutButton.propTypes = {
 	translate: PropTypes.func.isRequired
