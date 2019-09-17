1048c564f68f3482579d27510ddd201ef457e468
Artur Pawlak <pawlak.artur90@gmail.com>
2019-09-17 13:47:01 +0200

    Add prod config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const config = {
+const devConfig = {
 	apiKey: 'AIzaSyD0g1U-vlS6dYcWbogjbOtFzglFdgY14EY',
 	authDomain: 'cooking-book-b938a.firebaseapp.com',
 	databaseURL: 'https://cooking-book-b938a.firebaseio.com',
@@ -8,6 +8,19 @@ const config = {
 	storageBucket: 'cooking-book-b938a.appspot.com',
 	messagingSenderId: '674061107100'
 };
+
+const prodConfig = {
+	apiKey: 'AIzaSyCW74HHM_OQBsQz3KQ25f_nKRj2DoTFuoM',
+	authDomain: 'cooking-book-prod.firebaseapp.com',
+	databaseURL: 'https://cooking-book-prod.firebaseio.com',
+	projectId: 'cooking-book-prod',
+	storageBucket: '',
+	messagingSenderId: '324223952565',
+	appId: '1:324223952565:web:0e82a9bfecd0d04d07c0fc'
+};
+
+// eslint-disable-next-line no-undef
+const config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
 
 if (!firebase.apps.length) {
 	firebase.initializeApp(config);
