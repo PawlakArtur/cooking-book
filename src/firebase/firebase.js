a37d72995f944a6d2e26f8c96964d7605ea6e868
Artur Pawlak <pawlak.artur90@gmail.com>
2019-09-21 14:20:31 +0200

    Update firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,8 @@ const config = {
 	databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASE_URL,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 if (!firebase.apps.length) {
